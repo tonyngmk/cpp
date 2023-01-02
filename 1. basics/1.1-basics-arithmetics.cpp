33ec6355fb78d8b4fb4d6f8ce87d046a5a37f876
Tony <tonyngmengkiat@gmail.com>
2023-01-02 23:00:33 +0800

    add basic functions

--- 1. basics/1.1-basics-arithmetics.cpp
+++ 1. basics/1.1-basics-arithmetics.cpp
@@ -4,7 +4,71 @@ using namespace std;
 
 int main() 
 {
-    int x = 8 * 4;
-    cout << x;
-    return 0;
+    // // Basic
+    // cout << 1 + 2 << endl;
+    // cout << 3 - 2 << endl;
+    // cout << 8 * 4 << endl;
+    // cout << 8 / 3 << endl;
+    // cout << 8 % 3 << endl;
+
+    // Increment
+    // int a = 10;
+    // cout << a++ << endl;
+    // cout << ++a << endl;
+
+    // // Decrement
+    // int b = 15;
+    // cout << b-- << endl;
+    // cout << --b << endl;
+
+    // // Comparison
+    // cout << (6 == 4) << endl;
+    // cout << (4 == 4) << endl;
+    // cout << (6 >= 4) << endl;
+    // cout << (6 <= 4) << endl;
+    // cout << (6 != 4) << endl;
+
+    // // Logic
+    // cout << (0 && 0) << endl;
+    // cout << (0 && 1) << endl;
+    // cout << (1 && 1) << endl;
+
+    // cout << (0 || 0) << endl;
+    // cout << (0 || 1) << endl;
+    // cout << (1 || 1) << endl;
+
+    // cout << (! 0) << endl;
+    // cout << (! 1) << endl;
+
+    // // Bitwise
+    // cout << (5 & 4) << endl;
+    // cout << (5 | 4) << endl;
+
+    // cout << (0 ^ 0) << endl;
+    // cout << (1 ^ 1) << endl;
+    // cout << (1 ^ 0) << endl;
+
+    // cout << (1 << 16) << endl;
+
+    // cout << (1 << 16 >> 2) << endl;
+
+    // cout << (~ 4) << endl;
+
+
+    // // Assigment
+    // int a = 6, b = 4;
+
+    // cout << (a += b) << endl;
+    // cout << (a -= b) << endl;
+    // cout << (a *= b) << endl;
+    // cout << (a /= b) << endl;
+
+    // // Ternary operator
+    // int a = 6, b = 4;
+
+    // string result = (a > b) ? "yay" : "nay";
+    // cout << result << endl;
+
+    // return 0;
+
 }
